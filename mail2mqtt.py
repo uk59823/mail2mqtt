6953bb1026b00113f49b6167109187307b43fe1f
uk59823 <73341655+uk59823@users.noreply.github.com>
2021-09-01 14:10:01 +0200

    Add files via upload

--- mail2mqtt.py
+++ mail2mqtt.py
@@ -1,5 +1,7 @@
 import imaplib
 import email
+import configparser
+import json
 from email.header import decode_header
 import urllib.request
 import os
@@ -7,17 +9,11 @@ import time
 import datetime
 import paho.mqtt.client as mqtt
 
-
-# account credentials
-username = os.getenv('MAIL_USER', '')
-password = os.getenv('MAIL_PASSWORD', '')
-imapserver = os.getenv('MAIL_SERVER', '')
-interval = int(os.getenv('MAIL_INTERVAL', '60'))
-setseen = eval(os.getenv('MAIL_SEEN', 'True'))
-setdelete = eval(os.getenv('MAIL_DELETE', 'False'))
+broker = 'localhost'
+inifile = 'ini/mail2mqtt.ini'
 
 # MQTT-Settings
-mqtt_ipaddress = os.getenv('MQTT_BROKER', 'localhost')
+mqtt_ipaddress = os.getenv('MQTT_BROKER', broker)
 mqtt_user = os.getenv('MQTT_USER', '')
 mqtt_pass = os.getenv('MQTT_PASSWORD', '')
 mqtt_topic = os.getenv('MQTT_TOPIC', 'main_uk/mail')
@@ -26,9 +22,89 @@ mqtt_qos = int(os.getenv('MQTT_QOS', '2'))
 mqtt_retain = eval(os.getenv('MQTT_RETAIN', 'True'))
 mqtt_clientid = os.getenv('MQTT_CLIENTID', 'mail_mqtt')
 
-# Filter
-filterSubject = os.getenv('FILTER_SUBJECT', '')
-filterFrom = os.getenv('FILTER_FROM', '')
+postboxes = []
+filters = []
+
+
+def readIniValues():
+    # read settings from mail2mqtt.ini
+    ApplicationDir = os.path.dirname(os.path.abspath(__file__))
+    ReadSettings = os.path.join(ApplicationDir, inifile)
+    Settings = configparser.ConfigParser()
+    Settings.read(ReadSettings)
+    # existing postboxes
+    global interval, setseen
+    interval = int(Settings.get("General", "interval"))
+    setseen = eval(Settings.get("General", "todo_seen"))
+
+    # existing postboxes
+    global postboxes
+    pboxes = Settings.items("POSTBOX")
+
+    for id in pboxes:
+        postbox = {}
+        postbox['postboxname'] = str(id[1])
+        postbox['username'] = Settings.get(str(id[1]), "username")
+        postbox['password'] = Settings.get(str(id[1]), "password")
+        postbox['imapserver'] = Settings.get(str(id[1]), "server")
+        postboxes.append(postbox)
+
+    # existing filters
+    global filters
+    filter = Settings.items("FILTER")
+    for id in filter:
+        idfilter = {}
+        idfilter['filtername'] = str(id[1])
+        try:
+            idfilter['filterPostbox'] = Settings.get(
+                str(id[1]), "filter_postbox")
+        except:
+            idfilter['filterPostbox'] = ''
+        try:
+            idfilter['filterSubject'] = Settings.get(
+                str(id[1]), "filter_subject")
+        except:
+            idfilter['filterSubject'] = ''
+        try:
+            idfilter['filterFrom'] = Settings.get(str(id[1]), "filter_from")
+        except:
+            idfilter['filterFrom'] = ''
+        try:
+            idfilter['setdelete'] = eval(
+                Settings.get(str(id[1]), "todo_delete"))
+        except:
+            idfilter['setdelete'] = False
+        filters.append(idfilter)
+
+
+def checkFilter(pbox, subject, mail):
+    res = {}
+    for filter in filters:
+        r1 = False
+        r2 = False
+        r3 = False
+        if (filter['filterPostbox'] != ''):
+            for s in filter['filterPostbox'].lower().split('|'):
+                if s in pbox.lower():
+                    r1 = True
+        else:
+            r1 = True
+        if (filter['filterSubject'] != ''):
+            for s in filter['filterSubject'].lower().split('|'):
+                if (s in subject.lower()):
+                    r2 = True
+        else:
+            r2 = True
+        if (filter['filterFrom'] != ''):
+            for s in filter['filterFrom'].lower().split('|'):
+                if s in mail.lower():
+                    r3 = True
+        else:
+            r3 = True
+
+        if (r1 and r2 and r3):
+            res = filter
+    return res
 
 
 def connect():
@@ -79,7 +155,6 @@ def main():
     # create an IMAP4 class with SSL, use your email provider's IMAP server
 
     dt = datetime.datetime.now().astimezone().replace(microsecond=0).isoformat()
-    send_mqtt_paho(dt, mqtt_topic + "/update")
 
     if connect() != True:
         print("[" + dt + "] - No internet connection")
@@ -89,108 +164,143 @@ def main():
     else:
         send_mqtt_paho('200 OK', mqtt_topic + "/internet_response")
 
-    imap = imaplib.IMAP4_SSL(imapserver)
+    for postbox in postboxes:
 
-    try:
-        # authenticate
-        imap.login(username, password)
-    except imap.error as e:
-        print("[" + dt + "] - " + "Error: " + str(e))
-        send_mqtt_paho(str(e), mqtt_topic + "/error")
-        return "[" + dt + "] - Exit imap.login"
+        sum = []
 
-    # select a mailbox (in this case, the inbox mailbox)
-    # use imap.list() to get the list of mailboxes
-    status, messages = imap.select("INBOX")
-    status_unseen, messages_unseen = imap.search(None, 'UNSEEN')
+        subtopic = "/" + postbox['postboxname']
+        imap = imaplib.IMAP4_SSL(postbox['imapserver'])
 
-    # total number of emails
-    messages_total = int(messages[0])
+        try:
+            # authenticate
+            imap.login(postbox['username'], postbox['password'])
+        except imap.error as e:
+            print("[" + dt + "] - " + "Error: " + str(e))
+            send_mqtt_paho(str(e), mqtt_topic + subtopic + "/error")
+            return "[" + dt + "] - Exit imap.login"
 
-    send_mqtt_paho(interval, mqtt_topic + "/interval")
-    send_mqtt_paho(messages_total, mqtt_topic + "/total")
-    send_mqtt_paho(len(messages_unseen[0].split()), mqtt_topic + "/unread")
+        # select a mailbox (in this case, the inbox mailbox)
+        # use imap.list() to get the list of mailboxes
+        status, messages = imap.select("INBOX")
+        status_unseen, messages_unseen = imap.search(None, 'UNSEEN')
 
-    for i in messages_unseen[0].split():
-        # fetch the email message by ID
-        res, msg = imap.fetch(i, "BODY.PEEK[]")
-        for response in msg:
-            if isinstance(response, tuple):
+        # total number of emails
+        messages_total = int(messages[0])
 
-                subject = ""
-                From = ""
-                mail = ""
-                body = None
+        send_mqtt_paho(interval, mqtt_topic + subtopic + "/interval")
+        send_mqtt_paho(messages_total, mqtt_topic + subtopic + "/total")
+        send_mqtt_paho(
+            len(messages_unseen[0].split()), mqtt_topic + subtopic + "/unread")
 
-                # parse a bytes email into a message object
-                msg = email.message_from_bytes(response[1])
+        for i in messages_unseen[0].split():
+            # fetch the email message by ID
+            res, msg = imap.fetch(i, "BODY.PEEK[]")
+            for response in msg:
+                if isinstance(response, tuple):
 
-                # decode the email subject
-                subject_list = decode_header(msg['subject'])
-                subject = decodeElement(subject_list)
+                    subject = ""
+                    From = ""
+                    mail = ""
+                    body = None
 
-                # decode email-From sender
-                from_list = decode_header(msg.get("From"))
-                From = decodeElement(from_list)
+                    # parse a bytes email into a message object
+                    msg = email.message_from_bytes(response[1])
 
-                # decode email-Address sender
-                mail_list = decode_header(msg.get("Return-Path"))
-                mail = decodeElement(mail_list)
+                    # decode the email subject
+                    subject_list = decode_header(msg['subject'])
+                    subject = decodeElement(subject_list)
 
-                if (subject in filterSubject) or ((filterFrom != '') and (filterFrom in From)):
+                    # decode email-From sender
+                    from_list = decode_header(msg.get("From"))
+                    From = decodeElement(from_list)
 
-                    # if the email message is multipart
-                    if msg.is_multipart():
+                    # decode email-Address sender
+                    mail_list = decode_header(msg.get("Return-Path"))
+                    mail = decodeElement(mail_list)
+                    mail = mail.replace('<', '').replace('>', '')
 
-                        # iterate over email parts
-                        for part in msg.walk():
+                    # decode the email send date
+                    sent = decode_header(msg["Date"])[0][0]
+
+                    # decode the email receive date
+                    received = decode_header(msg['Received'])[
+                        0][0].split('; ')[1]
+
+                    filter = checkFilter(postbox['postboxname'], subject, mail)
+
+                    if filter != {}:
+
+                        # if the email message is multipart
+                        if msg.is_multipart():
+
+                            # iterate over email parts
+                            for part in msg.walk():
+                                # extract content type of email
+                                content_type = part.get_content_type()
+                                content_disposition = str(
+                                    part.get("Content-Disposition"))
+                                try:
+                                    # get the email body
+                                    if body != None:
+                                        body = body + '\n\n' + \
+                                            part.get_payload(
+                                                decode=True).decode()
+                                    else:
+                                        body = part.get_payload(
+                                            decode=True).decode()
+                                except:
+                                    pass
+                        else:
+
                             # extract content type of email
-                            content_type = part.get_content_type()
-                            content_disposition = str(
-                                part.get("Content-Disposition"))
-                            try:
-                                # get the email body
-                                if body != None:
-                                    body = body + '\n\n' + \
-                                        part.get_payload(decode=True).decode()
-                                else:
-                                    body = part.get_payload(
-                                        decode=True).decode()
-                            except:
-                                pass
-                    else:
+                            content_type = msg.get_content_type()
+                            # get the email body
+                            body = msg.get_payload(decode=True).decode()
 
-                        # extract content type of email
-                        content_type = msg.get_content_type()
-                        # get the email body
-                        body = msg.get_payload(decode=True).decode()
+                        if setseen == True:
+                            imap.store(i, '+FLAGS', '\Seen')
+                        if filter['setdelete'] == True:
+                            imap.store(mail, "+FLAGS", "\Deleted")
 
-                    if setseen == True:
-                        imap.store(i, '+FLAGS', '\Seen')
-                    if setdelete == True:
-                        imap.store(mail, "+FLAGS", "\Deleted")
+                        paho = {}
+                        paho['filter'] = filter['filtername']
+                        paho['from'] = From
+                        paho['mail'] = mail
+                        paho['subject'] = subject
+                        paho['body'] = body
+                        paho['sent'] = sent
+                        paho['received'] = received
 
-                    print(dt + " - gefilterte Mail von '" + From + "'")
-                    send_mqtt_paho(From, mqtt_topic + "/from")
-                    send_mqtt_paho(mail, mqtt_topic + "/mail")
-                    send_mqtt_paho(subject, mqtt_topic + "/subject")
-                    send_mqtt_paho(body, mqtt_topic + "/body")
+                        sum.append(paho)
 
-    # close the connection and logout
-    imap.close()
-    imap.logout()
+        if sum == []:
+            count = 0
+        else:
+            count = len(sum)
+            print(dt + " - gefilterte Mail von '" +
+                  postbox['postboxname'] + "': " + str(count))
+        send_mqtt_paho(count, mqtt_topic + subtopic + "/count")
+        send_mqtt_paho(json.dumps(sum), mqtt_topic + subtopic + "/found")
+
+        # close the connection and logout
+        imap.close()
+        imap.logout()
+
+    send_mqtt_paho(dt, mqtt_topic + "/update")
     return "[" + dt + "] - Search ended"
 
 
 if __name__ == '__main__':
     print("mail2mqtt started")
+    readIniValues()
+
     while 1:
 
         dtStart = datetime.datetime.now()
         r = main()
         print(r)
         dtEnd = datetime.datetime.now()
-        sleeptime = interval - (dtEnd - dtStart).total_seconds()
+        sleeptime = (60 * interval) - (dtEnd - dtStart).total_seconds()
         if sleeptime < 0:
-            sleeptime = interval
+            sleeptime = 60 * interval
         time.sleep(sleeptime)
